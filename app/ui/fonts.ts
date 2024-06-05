4713b0d6d3110199e584f21148ff3a2e626d72a3
Erick Jose Reyes Pineda <erick.reyes@ladylee.com>
2024-06-05 16:40:51 -0600

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Poppins, Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ['latin'] });
+export const poppins = Poppins({ weight: ['400'], subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400'], subsets: ['latin'] });
